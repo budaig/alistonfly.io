4eec7a2179abf037e3f1087acbf12dcd32eff05a
B1uEx <brux@qq.com>
2024-07-30 22:12:15 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ APP_NAME=alistpan
 
 if ! command -v flyctl >/dev/null 2>&1; then
     printf '\e[33m进度1/5：安装Fly.io CLI。\n\e[0m'
-    curl -L https://fly.io/install.sh
+    curl -L https://fly.io/install.sh | sh
 fi
 
 if [ -z "${APP_NAME}" ]; then
@@ -62,13 +62,13 @@ EOF
 printf '\e[32m成功创建配置\n\e[0m'
 printf '\e[33m进度4/5：创建环境变量及部署区域\n\e[0m'
 
-fly secrets set DATABASE="$DATABASE"
+flyctl secrets set DATABASE="$DATABASE"
 # flyctl secrets set SQLUSER="${SQLUSER}"
 # flyctl secrets set SQLPASSWORD="${SQLPASSWORD}"
 # flyctl secrets set SQLHOST="${SQLHOST}"
 # flyctl secrets set SQLPORT="${SQLPORT}"
 # flyctl secrets set SQLNAME="${SQLNAME}"
-fly platform regions $REGION
+flyctl platform regions $REGION
 printf '\e[32m进度5/5：部署\n\e[0m'
-fly deploy --detach
+flyctl deploy --detach
 # flyctl status --app ${APP_NAME}
